d28222540356a9ca40642376b8342941c288b6b2
Alexandre <alexandredmartinz@gmail.com>
2021-07-28 09:09:13 -0300

    Criado o App diligencias, criada a classe Diligencia

--- endereco/models.py
+++ endereco/models.py
@@ -9,3 +9,6 @@ class Endereco(models.Model):
     numero = models.IntegerField(null=False, blank=False)
     cep = models.CharField(max_length=10, null=False, blank=False)
 
+    def __str__(self):
+        return self.logradouro
+
--- operacoes/settings.py
+++ operacoes/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'endereco.apps.EnderecoConfig',
+    'pessoas.apps.PessoasConfig',
+    'diligencias.apps.DiligenciasConfig'
 ]
 
 MIDDLEWARE = [
@@ -119,6 +121,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
